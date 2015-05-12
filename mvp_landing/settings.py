d027c0eaa3b5e25ed9a771aed5ec0054749b5db5
alex <dolgiyspb@gmail.com>
2015-05-12 20:47:31 +0400

    Remove snippetscream

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import snippetscream
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -42,7 +42,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snippetscream',
     'signups',
 )
 CREATE_DEFAULT_SUPERUSER = True
